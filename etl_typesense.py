4bf09ca68534f550225b4fab293cec3693c2ee49
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-06-25 17:33:55 -0400

    test code

--- etl_typesense.py
+++ etl_typesense.py
@@ -21,8 +21,8 @@ from typesense.exceptions import ObjectNotFound
 
 
 
-#make the client 
-client = typesense.Client({
+#make the typesense client 
+typesense_client = typesense.Client({
   'api_key': API_KEY,
   'nodes': [{
     'host': 'localhost',
@@ -32,10 +32,14 @@ client = typesense.Client({
   'connection_timeout_seconds': 2
 })
 
+#make the mongo cleint
+mongo_client = mongo_helper_kit.create_mongo_client(MONGO_HOST_NAME)
 
 
+
+#try to check for the client
 try:
-    client.collections['articles'].delete()
+    typesense_client.collections['articles'].delete()
     print("Collection 'articles' deleted.")
 except ObjectNotFound:
     print("Collection 'articles' does not exist. Skipping deletion.")
@@ -44,12 +48,13 @@ except Exception as e:
 
 
 
+
 def check_client():
     """
     The function to check the client health
     """
     try:
-        collections = client.collections.retrieve()
+        collections = typesense_client.collections.retrieve()
         print("Connected successfully!")
         return True
 
@@ -57,6 +62,8 @@ def check_client():
         print("Connection failed!")
         print("Error:", e)
         return False
+
+
 
 
 def check_container_health():
@@ -76,31 +83,7 @@ def check_container_health():
     except requests.exceptions.RequestException as e:
         print(f"Error connecting to Typesense: {e}")
         return False
-
-
-
-#check the container health
-print(check_container_health())
-
-#check the client container
-print(check_client())
-
-
-
-#make the schema
-#this is the nested schema 
-#other stragety can be using the flatten the data and then use it 
-
-
-#create the collection
-#if not check_container_health and not check_client(): 
-
-client.collections.create(ARTICLE_SCHEMA)
-
-#get the schema
-retrieve_response = client.collections['articles'].retrieve()
-print(retrieve_response)
-
+    
 
 # Convert _id to id as string, keep the rest unchanged
 def convert_id(doc):
@@ -109,32 +92,70 @@ def convert_id(doc):
     return doc
 
 
-#make the mongo cleint
-mongo_client = mongo_helper_kit.create_mongo_client(MONGO_HOST_NAME)
-
-#the db name
-db = mongo_client[DB_NAME]
-collection = db[COLLECTION_NAME]
 
 
-# Example: Fetching documents from MongoDB
-mongo_documents = list(collection.find({}))
 
-#get all mongo data
-#print(mongo_documents)
+def ingest_data():
+    """
+    The main function to run the logic for ingestion
+    """
+
+    #check the container and the client 
+
+    container_health = check_container_health()
+
+    if not container_health:
+
+        return "The contaienr is not working"
+    
+    client_health = check_client()
+
+    if not client_health :
+
+        return "The client is not initilaized properly"
+    
+    #create the article schema
+    typesense_client.collections.create(ARTICLE_SCHEMA)
+
+    #get the schema
+    #retrieve_response = typesense_client.collections['articles'].retrieve()
+    #print(retrieve_response)
 
 
-# Convert _id to string for Typesense
-cleaned_docs = [convert_id(doc) for doc in mongo_documents]
+    #the db name
+    db = mongo_client[DB_NAME]
+    collection = db[COLLECTION_NAME]
 
-#insert the data into the typesnse 
+    # Example: Fetching documents from MongoDB
+    mongo_documents = list(collection.find({}))
 
-response = client.collections['articles'].documents.import_(
-    cleaned_docs,
-    {'action': 'upsert'}  # 'upsert' means insert or update
-)
+    #get all mongo data
+    #print(mongo_documents)
 
 
-#export the data and check
-export_output = client.collections['articles'].documents.export()
-print(export_output)
+    # Convert _id to string for Typesense
+    cleaned_docs = [convert_id(doc) for doc in mongo_documents]
+
+    #insert the data into the typesnse 
+    response = typesense_client.collections['articles'].documents.import_(
+        cleaned_docs,
+        {'action': 'upsert'}  # 'upsert' means insert or update
+    )
+
+
+    #export the data and check
+    export_output = typesense_client.collections['articles'].documents.export()
+    print(export_output)
+
+    return True
+
+
+
+if __name__ == "__main__":
+
+    ingest_data()
+
+
+
+
+
